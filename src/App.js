e84514d496c15a5377cfcea1d7d662a5c642c25d
Alejandro Montano <alejandromontano@useit.co>
2023-03-19 17:25:30 -0500

    Login to backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [message, setMessage] = useState("");
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:9000/api/login",
+        user
+      );
+      setMessage(response.data.message);
+    } catch (error) {
+      setMessage(error.response.data.message);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-4 offset-md-4">
+          <div className="card">
+            <div className="card-body">
+              <h3 className="card-title text-center mb-4">Login</h3>
+              <form onSubmit={handleFormSubmit}>
+                <div className="mb-3">
+                  <label htmlFor="username" className="form-label">
+                    username address
+                  </label>
+                  <input
+                    type="username"
+                    className="form-control"
+                    id="username"
+                    name="username"
+                    value={user.username}
+                    onChange={handleInputChange}
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="password" className="form-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    name="password"
+                    value={user.password}
+                    onChange={handleInputChange}
+                    required
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary w-100">
+                  Login
+                </button>
+              </form>
+              <div className="mt-3 text-center">
+                {message && <p className="text-danger">{message}</p>}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
